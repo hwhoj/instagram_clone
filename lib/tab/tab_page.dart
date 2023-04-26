c2692584d6e33b7668812503b715a72aee65d137
hwhoj <hwhoj@naver.com>
2023-04-26 14:44:01 +0900

    3강 TabPage에 화면결

--- lib/tab/tab_page.dart
+++ lib/tab/tab_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/tab/account/account_page.dart';
+import 'package:instagram_clone/tab/home/home_page.dart';
+import 'package:instagram_clone/tab/search/search_page.dart';
 
 class TabPage extends StatefulWidget {
   const TabPage({Key? key}) : super(key: key);
@@ -10,9 +13,16 @@ class TabPage extends StatefulWidget {
 class _TabPageState extends State<TabPage> {
   int _currentIndex = 0;
 
+  final _pages = const [
+    HomePage(),
+    SearchPage(),
+    AccountPage(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: _pages[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentIndex,
         onTap: (index){
