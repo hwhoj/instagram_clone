577e392ffc5c7ca0f10ae0beb59f58f09f9caff1
hwhoj <hwhoj@naver.com>
2023-04-27 14:06:41 +0900

    5강 AccountPage UI 작성

--- lib/tab/account/account_page.dart
+++ lib/tab/account/account_page.dart
@@ -5,6 +5,112 @@ class AccountPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Instagram Clone'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.exit_to_app),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              children: [
+                Stack(
+                  //사진위에 버튼을 겹쳐놓아야 하기때문에 사진이있는 사이즈박스를 스택으로 감싸줌
+                  children: [
+                    const SizedBox(
+                      width: 80,
+                      height: 80,
+                      child: CircleAvatar(
+                        backgroundImage: NetworkImage(
+                            'https://dimg.donga.com/wps/NEWS/IMAGE/2022/08/17/114998051.2.jpg'),
+                      ),
+                    ),
+                    Container(
+                      width: 80,
+                      height: 80,
+                      alignment: Alignment.bottomRight,
+                      child: const Stack(
+                        //+아이콘 바깥으로 흰색바탕을 만들어주기위한 스택
+                        alignment: Alignment.center,
+                        children: [
+                          SizedBox(
+                            width: 28,
+                            height: 28,
+                            child: FloatingActionButton(
+                              onPressed: null,
+                              backgroundColor: Colors.white,
+                            ),
+                          ),
+                          SizedBox(
+                            width: 25,
+                            height: 25,
+                            child: FloatingActionButton(
+                              onPressed: null,
+                              child: Icon(Icons.add),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 8),
+                const Text(
+                  '이름',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            const Column(
+              children: [
+                Text(
+                  '3',
+                  style: TextStyle(fontSize: 18),
+                ),
+                Text(
+                  '게시물',
+                  style: TextStyle(fontSize: 18),
+                ),
+              ],
+            ),
+            const Column(
+              children: [
+                Text(
+                  '0',
+                  style: TextStyle(fontSize: 18),
+                ),
+                Text(
+                  '팔로워',
+                  style: TextStyle(fontSize: 18),
+                ),
+              ],
+            ),
+            const Column(
+              children: [
+                Text(
+                  '0',
+                  style: TextStyle(fontSize: 18),
+                ),
+                Text(
+                  '팔로잉',
+                  style: TextStyle(fontSize: 18),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
